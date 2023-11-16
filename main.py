f0f3fa9b18ef9d112fe217b708aaaece6e992934
Jonasz Kiepe <loafofbread63@gmail.com>
2023-11-16 21:54:29 +0100

    Update

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI
+import pandas as pd
 from pydantic import BaseModel
+from fastapi import FastAPI
+from datetime import datetime, timedelta
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+class Rental(BaseModel):
+    equipment: list
+    start_time: datetime
+    end_time: datetime
+    forward_payment: float
+
+
+def calculate_duration(start, end):
+    datetime.DeltaTime()
+
+
+price_list = pd.read_json("price.json")
 
 
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item.price += 100000
-    return item
+@app.post('/calc/price')
+async def price_calc(rental: Rental):
+    return rental
+
