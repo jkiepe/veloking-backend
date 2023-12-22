cacdf0bb3d980cc71c75f78a80ea1a844e7956af
Jonasz <loafofbread63@gmail.com>
2023-12-22 08:59:01 +0100

    environment change

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import uvicorn
+from decouple import config
+
+HOST = config("apihost")
+PORT = config("apiport")
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="localhost", port=8080, reload=True)
+    uvicorn.run("app.api:app", host=HOST, port=PORT, reload=True)
