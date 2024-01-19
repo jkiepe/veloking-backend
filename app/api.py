aab3d4bfba73a1560d748ce21aeed7122dedf7e8
Jonasz <loafofbread63@gmail.com>
2024-01-19 08:01:13 +0100

    nothing

--- app/api.py
+++ app/api.py
@@ -1,14 +1,12 @@
-from typing import Annotated
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from app import crud, database
-from app.models import tables, schemas
 from app.auth import auth_bearer, jwt_handler
+from app.models import schemas
 
 database.setup()
-
 
 def get_db():
     db = database.SessionLocal()
@@ -34,8 +32,7 @@ async def home():
 
 
 @app.post("/user/login", tags=["user"])
-async def user_login(login_data: schemas.LoginSchema,
-                     db: Session = Depends(get_db)):
+async def user_login(login_data: schemas.LoginSchema, db: Session = Depends(get_db)):
     user = crud.user_get_by_username(login_data.username, db)
     if user:
         if user.disabled:
@@ -46,8 +43,7 @@ async def user_login(login_data: schemas.LoginSchema,
 
 
 @app.post("/user/create", tags=["user"])
-async def user_create(user: schemas.UserSchema,
-                      db: Session = Depends(get_db)):
+async def user_create(user: schemas.UserSchema, db: Session = Depends(get_db)):
     if crud.user_get_by_username(user.username, db):
         raise HTTPException(status_code=400, detail="Username already registered")
     crud.user_create(user, db)
@@ -61,8 +57,9 @@ async def user_list(db: Session = Depends(get_db)):
 
 
 @app.get("/user/me", tags=["user"])
-async def user_get_myself(token: str = Depends(auth_bearer.JWTBearer()),
-                          db: Session = Depends(get_db)):
+async def user_get_myself(
+    token: str = Depends(auth_bearer.JWTBearer()), db: Session = Depends(get_db)
+):
     payload = jwt_handler.decode(token)
     username = payload["username"]
     user = crud.user_get_by_username(username, db)
@@ -70,12 +67,13 @@ async def user_get_myself(token: str = Depends(auth_bearer.JWTBearer()),
 
 
 @app.put("/user/point", tags=["user"])
-async def user_move_to_point(username: str,
-                             point_key: str,
-                             db: Session = Depends(get_db)):
+async def user_move_to_point(
+    username: str, point_key: str, db: Session = Depends(get_db)
+):
     user = crud.user_get_by_username(username, db)
     point = crud.point_get_by_point_key(point_key, db)
     crud.user_move_to_point(user, point, db)
+
 
 @app.put("/price/create", tags=["price"])
 async def price_create(price: schemas.PriceSchema, db: Session = Depends(get_db)):
@@ -83,46 +81,52 @@ async def price_create(price: schemas.PriceSchema, db: Session = Depends(get_db)
 
 
 @app.post("/point/create", tags=["point"])
-async def point_create(point: schemas.PointSchema,
-                       db: Session = Depends(get_db)):
+async def point_create(point: schemas.PointSchema, db: Session = Depends(get_db)):
     if crud.point_get_by_point_key(point.point_key, db):
         raise HTTPException(status_code=400, detail="Point already registered")
-    data.point_create(point, db)
+    crud.point_create(point, db)
 
 
 @app.get("/point/list", tags=["point"])
 async def point_list(db: Session = Depends(get_db)):
     points = crud.point_get_all(db)
-    points = [{
-        "point_key": point.point_key,
-       "name": point.name,
-       } for point in points]
+    points = [
+        {
+            "point_key": point.point_key,
+            "name": point.name,
+        }
+        for point in points
+    ]
     return {"points": points}
 
 
-@app.post("/rental/prepayment", tags=["rental"])
-async def rental_calculate_prepayment(prepayment: schemas.PrepaymentSchema,
-                                      db: Session = Depends(get_db)):
-    return crud.rental_calculate_price(prepayment, db)
+# @app.post("/rental/prepayment", tags=["rental"])
+# async def rental_calculate_prepayment(
+#     prepayment: schemas.PrepaymentSchema, db: Session = Depends(get_db)
+# ):
+#     return crud.rental_calculate_price(prepayment, db)
 
 
 @app.get("/vehicle/list", tags=["vehicle"])
 async def vehicle_list(db: Session = Depends(get_db)):
     vehicles = crud.vehicle_get_all(db)
-    vehicles = [{
-        "superior_category": vehicle.superior_category,
-        "sub_category": vehicle.sub_category,
-        "vehicle_key": vehicle.vehicle_key,
-        "rented": vehicle.rented,
-        "rental_point": vehicle.rental_point.point_key
-        } for vehicle in vehicles]
+    vehicles = [
+        {
+            "superior_category": vehicle.superior_category,
+            "sub_category": vehicle.sub_category,
+            "vehicle_key": vehicle.vehicle_key,
+            "rented": vehicle.rented,
+            "rental_point": vehicle.rental_point.point_key,
+        }
+        for vehicle in vehicles
+    ]
     return {"vehicles": vehicles}
 
 
 @app.get("/vehicle/move", tags=["vehicle"])
 async def vehicle_move(vehicle_key: str, point_key: str, db: Session = Depends(get_db)):
     crud.vehicle_move(vehicle_key, point_key, db)
-    
+
 
 # @app.post("/rental/create", tags=["rental"])
 # async def rental_create(rental: schemas.RentalSchema,
--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import select
 
 from app.database import crypt
-from app.models import tables, schemas
+from app.models import schemas, tables
 
 
 def user_verify(password, hash_password):
@@ -49,24 +48,29 @@ def point_get_all(database: Session):
 
 
 def point_get_by_key(point_key: str, database: Session):
-    return database.query(tables.Point).filter(tables.Point.point_key == point_key).first()
+    return (
+        database.query(tables.Point).filter(tables.Point.point_key == point_key).first()
+    )
 
 
-def vehicle_create(vehicle: schemas.VehicleSchema,
-                   database: Session):
-    office = database.query(tables.Point).filter(tables.Point.point_key == "office").first()
+def vehicle_create(vehicle: schemas.VehicleSchema, database: Session):
+    office = (
+        database.query(tables.Point).filter(tables.Point.point_key == "office").first()
+    )
     new_vehicle = tables.Vehicle(**vehicle.dict(), rental_point=office)
     database.add(new_vehicle)
     database.commit()
 
 
 def vehicle_get_by_key(vehicle_key: str, database: Session):
-    return database.query(tables.Vehicle).filter(tables.Vehicle.vehicle_key == vehicle_key).first()
+    return (
+        database.query(tables.Vehicle)
+        .filter(tables.Vehicle.vehicle_key == vehicle_key)
+        .first()
+    )
 
 
-def vehicle_move(vehicle_key: str,
-                 point_key: str,
-                 database: Session):
+def vehicle_move(vehicle_key: str, point_key: str, database: Session):
     vehicle = vehicle_get_by_key(vehicle_key, database)
     point = point_get_by_key(point_key, database)
     vehicle.rental_point = point
@@ -90,5 +94,5 @@ def vehicle_get_all(database: Session):
 #             price += prepayment.half_hour * price_list.half_hour
 #             price += prepayment.hour * price_list.hour
 #             price += prepayment.per_24_hours * price_list.per_24_hours
-#             
+#
 #
